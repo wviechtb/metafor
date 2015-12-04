4978818d39c0f916939e74772ce70a7cad6d21c4
Wolfgang Viechtbauer <wvb@wvbauer.com>
2015-12-04 17:39:22 +0100

    Added cooks.distance.rma.mv(), so now cooks.distance() also works with 'rma.mv' objects.

---
+++ R/cooks.distance.rma.mv.r
@@ -0,0 +1,66 @@
+cooks.distance.rma.mv <- function(model, ...) {
+
+   if (!is.element("rma.mv", class(model)))
+      stop("Argument 'model' must be an object of class \"rma.mv\".")
+
+   na.act <- getOption("na.action")
+
+   if (!is.element(na.act, c("na.omit", "na.exclude", "na.fail", "na.pass")))
+      stop("Unknown 'na.action' specified under options().")
+
+   x <- model
+
+   if (x$k == 1)
+      stop("Stopped because k = 1.")
+
+   #########################################################################
+
+   cook.d <- rep(NA_real_, x$k.f)
+
+   ### calculate inverse of variance-covariance matrix under the full model (needed for the Cook's distances)
+
+   svb <- chol2inv(chol(x$vb))
+
+   ### note: skipping NA cases
+   ### also: it is possible that model fitting fails, so that generates more NAs (these NAs will always be shown in output)
+
+   for (i in seq_len(x$k.f)[x$not.na]) {
+
+      res <- try(suppressWarnings(rma.mv(x$yi.f[-i], x$V.f[-i,-i], W=x$W.f[-i,-i], mods=cbind(x$X.f[-i,]), random=x$random, struct=x$struct, intercept=FALSE, method=x$method, tdist=x$knha, R=x$R, Rscale=x$Rscale, data=lapply(x$mf.r, function(x) x[-i,,drop=FALSE]), sigma2=ifelse(x$vc.fix$sigma2, x$sigma2, NA), tau2=ifelse(x$vc.fix$tau2, x$tau2, NA), rho=ifelse(x$vc.fix$rho, x$rho, NA), gamma2=ifelse(x$vc.fix$gamma2, x$gamma2, NA), phi=ifelse(x$vc.fix$phi, x$phi, NA), control=x$control)), silent=TRUE)
+
+      if (inherits(res, "try-error"))
+         next
+
+      ### removing an observation could lead to a model coefficient becoming inestimable
+
+      if (any(res$coef.na))
+         next
+
+      ### compute dfbeta value(s)
+
+      dfb <- x$b - res$b
+
+      ### compute Cook's distance
+
+      cook.d[i]  <- crossprod(dfb,svb) %*% dfb
+
+   }
+
+   #########################################################################
+
+   if (na.act == "na.omit") {
+      out <- cook.d[x$not.na]
+      names(out) <- x$slab[x$not.na]
+   }
+
+   if (na.act == "na.exclude" || na.act == "na.pass") {
+      out <- cook.d
+      names(out) <- x$slab
+   }
+
+   if (na.act == "na.fail" && any(!x$not.na))
+      stop("Missing values in results.")
+
+   return(out)
+
+}
