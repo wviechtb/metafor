d9bc3c1b8350dfe751e3041a4b6baa504068672b
Wolfgang Viechtbauer <wvb@wvbauer.com>
2017-03-17 20:03:04 +0100

    Added 'type' argument/functionality to residuals().

--- R/residuals.rma.r
+++ R/residuals.rma.r
@@ -1,4 +1,4 @@
-residuals.rma <- function(object, ...) {
+residuals.rma <- function(object, type="response", ...) {
 
    if (!inherits(object, "rma"))
       stop("Argument 'object' must be an object of class \"rma\".")
@@ -8,26 +8,86 @@ residuals.rma <- function(object, ...) {
    if (!is.element(na.act, c("na.omit", "na.exclude", "na.fail", "na.pass")))
       stop("Unknown 'na.action' specified under options().")
 
-   #########################################################################
+   type <- match.arg(type, c("response", "rstandard", "rstudent", "pearson", "cholesky"))
 
-   x <- object
+   ### for objects of class "rma.mh" and "rma.peto", use rstandard() to get the Pearson residuals
 
-   ### note: can calculate this even if vi is missing
-
-   out <- c(x$yi.f - x$X.f %*% x$beta)
-   out[abs(out) < 100 * .Machine$double.eps] <- 0
+   if (inherits(object, c("rma.mh", "rma.peto")) && type == "pearson")
+      type <- "rstandard"
 
    #########################################################################
 
-   names(out) <- x$slab
+   if (type == "rstandard") {
+      tmp <- rstandard(object)
+      out <- c(tmp$z)
+      names(out) <- tmp$slab
+   }
 
-   not.na <- !is.na(out)
+   if (type == "rstudent") {
+      tmp <- rstudent(object)
+      out <- c(tmp$z)
+      names(out) <- tmp$slab
+   }
 
-   if (na.act == "na.omit")
-      out <- out[not.na]
+   #########################################################################
 
-   if (na.act == "na.fail" && any(!not.na))
-      stop("Missing values in results.")
+   if (type == "response") {
+
+      ### note: can calculate this even if vi is missing
+
+      out <- c(object$yi.f - object$X.f %*% object$beta)
+      out[abs(out) < 100 * .Machine$double.eps] <- 0
+
+   }
+
+   if (type == "pearson") {
+
+      if (inherits(object, c("rma.mh", "rma.peto", "rma.glmm")))
+         stop("Extraction of Pearson residuals not implemented for objects of class \"rma.mh\", \"rma.peto\", or \"rma.glmm\".")
+
+      out <- c(object$yi.f - object$X.f %*% object$beta)
+      out[abs(out) < 100 * .Machine$double.eps] <- 0
+
+      se <- rep(NA_real_, object$k.f)
+      se[object$not.na] <- sqrt(diag(object$M))
+
+      out <- out / se
+
+   }
+
+   if (type == "cholesky") {
+
+      ### note: Cholesky residuals depend on the data order
+
+      if (inherits(object, c("rma.mh", "rma.peto", "rma.glmm")))
+         stop("Extraction of Cholesky residuals not implemented for objects of class \"rma.mh\", \"rma.peto\", or \"rma.glmm\".")
+
+      out <- c(object$yi - object$X %*% object$beta)
+      out[abs(out) < 100 * .Machine$double.eps] <- 0
+
+      L <- try(chol(chol2inv(chol(object$M))))
+      tmp <- L %*% out
+
+      out <- rep(NA_real_, object$k.f)
+      out[object$not.na] <- tmp
+
+   }
+
+   if (is.element(type, c("response", "pearson", "cholesky"))) {
+
+      names(out) <- object$slab
+
+      not.na <- !is.na(out)
+
+      if (na.act == "na.omit")
+         out <- out[not.na]
+
+      if (na.act == "na.fail" && any(!not.na))
+         stop("Missing values in results.")
+
+   }
+
+   #########################################################################
 
    return(out)
 
--- tests/testthat/test_misc_residuals.r
+++ tests/testthat/test_misc_residuals.r
@@ -12,6 +12,10 @@ test_that("residuals are correct for rma().", {
    expect_equivalent(round(rstandard(res)$z, 4), c(0.1401, -0.9930, -0.4719, -1.0475, 1.6462, 0.4825))
    expect_equivalent(round(rstudent(res)$z, 4),  c(0.1426, -0.9957, -0.4591, -1.1949, 2.0949, 0.4330))
 
+   res <- rma(yi, vi, data=dat, method="FE")
+   expect_equivalent(round(sum(residuals(res, type="pearson")^2), 4), round(res$QE, 4))
+   expect_equivalent(round(sum(residuals(res, type="cholesky")^2), 4), round(res$QE, 4))
+
 })
 
 test_that("residuals are correct for rma.mv().", {
@@ -32,8 +36,8 @@ test_that("residuals are correct for rma.mh().", {
 
    res <- rma.mh(measure="RR", ai=tpos, bi=tneg, ci=cpos, di=cneg, data=dat.bcg, subset=1:6)
    expect_equivalent(c(residuals(res)), c(dat$yi - coef(res)))
-   expect_equivalent(round(rstandard(res)$z, 4), c(0.1068, -1.4399, -0.6173, -3.4733, 3.2377, 1.9749))
-   expect_equivalent(round(rstudent(res)$z, 4),  c(0.1076, -1.4668, -0.6219, -4.2413, 3.3947, 2.7908))
+   expect_equivalent(round(residuals(res, type="rstandard"), 4), c(0.1068, -1.4399, -0.6173, -3.4733, 3.2377, 1.9749))
+   expect_equivalent(round(residuals(res, type="rstudent"), 4),  c(0.1076, -1.4668, -0.6219, -4.2413, 3.3947, 2.7908))
 
 })
 
