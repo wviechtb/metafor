8e448a6f9e383c609ab6a0b604f0f96b9679df9f
Wolfgang Viechtbauer <wvb@wvbauer.com>
2017-04-24 17:54:58 +0200

    Updated test for forest plot with subgroups. [ci skip]

--- tests/testthat/test_plots_forest_plot_with_subgroups.r
+++ tests/testthat/test_plots_forest_plot_with_subgroups.r
@@ -26,7 +26,13 @@ test_that("plot can be drawn.", {
           ilab=cbind(dat.bcg$tpos, dat.bcg$tneg, dat.bcg$cpos, dat.bcg$cneg),
           ilab.xpos=c(-9.5,-8,-6,-4.5), cex=.75, ylim=c(-1, 27),
           order=order(dat.bcg$alloc), rows=c(3:4,9:15,20:23),
-          xlab="Risk Ratio", mlab="RE Model for All Studies", psize=1)
+          xlab="Risk Ratio", mlab="", psize=1)
+
+   ### add text with Q-value, dfs, p-value, and I^2 statistic
+   text(-16, -1, pos=4, cex=0.75, bquote(paste("RE Model for All Studies (Q = ",
+        .(formatC(res$QE, digits=2, format="f")), ", df = ", .(res$k - res$p),
+        ", p = ", .(formatC(res$QEp, digits=2, format="f")), "; ", I^2, " = ",
+        .(formatC(res$I2, digits=1, format="f")), "%)")))
 
    ### set font expansion factor (as in forest() above) and use bold italic
    ### font and save original settings in object 'op'
@@ -58,9 +64,23 @@ test_that("plot can be drawn.", {
                 subset=(alloc=="alternate"), method="REML")
 
    ### add summary polygons for the three subgroups
-   addpoly(res.s, row=18.5, cex=.75, atransf=exp, mlab="RE Model for Subgroup")
-   addpoly(res.r, row= 7.5, cex=.75, atransf=exp, mlab="RE Model for Subgroup")
-   addpoly(res.a, row= 1.5, cex=.75, atransf=exp, mlab="RE Model for Subgroup")
+   addpoly(res.s, row=18.5, cex=0.75, atransf=exp, mlab="")
+   addpoly(res.r, row= 7.5, cex=0.75, atransf=exp, mlab="")
+   addpoly(res.a, row= 1.5, cex=0.75, atransf=exp, mlab="")
+
+   ### add text with Q-value, dfs, p-value, and I^2 statistic for subgroups
+   text(-16, 18.5, pos=4, cex=0.75, bquote(paste("RE Model for Subgroup (Q = ",
+        .(formatC(res.s$QE, digits=2, format="f")), ", df = ", .(res.s$k - res.s$p),
+        ", p = ", .(formatC(res.s$QEp, digits=2, format="f")), "; ", I^2, " = ",
+        .(formatC(res.s$I2, digits=1, format="f")), "%)")))
+   text(-16, 7.5, pos=4, cex=0.75, bquote(paste("RE Model for Subgroup (Q = ",
+        .(formatC(res.r$QE, digits=2, format="f")), ", df = ", .(res.r$k - res.r$p),
+        ", p = ", .(formatC(res.r$QEp, digits=2, format="f")), "; ", I^2, " = ",
+        .(formatC(res.r$I2, digits=1, format="f")), "%)")))
+   text(-16, 1.5, pos=4, cex=0.75, bquote(paste("RE Model for Subgroup (Q = ",
+        .(formatC(res.a$QE, digits=2, format="f")), ", df = ", .(res.a$k - res.a$p),
+        ", p = ", .(formatC(res.a$QEp, digits=2, format="f")), "; ", I^2, " = ",
+        .(formatC(res.a$I2, digits=1, format="f")), "%)")))
 
    par(opar)
 
