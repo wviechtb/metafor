ea427e341b62081e6301b5be48b002664f54a0ff
Wolfgang Viechtbauer <wvb@wvbauer.com>
2017-06-22 19:24:37 +0200

    Version increase to 2.1-0 for devel version. [ci skip]

--- R/zzz.r
+++ R/zzz.r
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname) {
-   loadmsg <- "Loading 'metafor' package (version 2.0-0). For an overview \nand introduction to the package please type: help(metafor)."
+   loadmsg <- "Loading 'metafor' package (version 2.1-0). For an overview \nand introduction to the package please type: help(metafor)."
    packageStartupMessage(loadmsg, domain=NULL, appendLF=TRUE)
 }
