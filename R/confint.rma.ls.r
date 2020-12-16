3f4ee030f50d354b0870ddaed012958f752b1596
Wolfgang Viechtbauer <wvb@wvbauer.com>
2020-12-16 22:47:24 +0100

    Added confint() method for location-scale models.

---
+++ R/confint.rma.ls.r
@@ -0,0 +1,372 @@
+confint.rma.ls <- function(object, parm, level, fixed=FALSE, alpha, digits, transf, targs, verbose=FALSE, control, ...) {
+
+   mstyle <- .get.mstyle("crayon" %in% .packages())
+
+   if (!inherits(object, "rma.ls"))
+      stop(mstyle$stop("Argument 'object' must be an object of class \"rma.ls\"."))
+
+   if (!missing(parm))
+      warning(mstyle$warning("Argument 'parm' (currently) ignored."), call.=FALSE)
+
+   x <- object
+
+   k <- x$k
+   p <- x$p
+
+   if (missing(level))
+      level <- x$level
+
+   if (missing(digits)) {
+      digits <- .get.digits(xdigits=x$digits, dmiss=TRUE)
+   } else {
+      digits <- .get.digits(digits=digits, xdigits=x$digits, dmiss=FALSE)
+   }
+
+   if (missing(transf))
+      transf <- FALSE
+
+   if (missing(targs))
+      targs <- NULL
+
+   if (missing(control))
+      control <- list()
+
+   level <- ifelse(level == 0, 1, ifelse(level >= 1, (100-level)/100, ifelse(level > .5, 1-level, level)))
+
+   ddd <- list(...)
+
+   .chkdots(ddd, c("time"))
+
+   if (.isTRUE(ddd$time))
+      time.start <- proc.time()
+
+   ### check if user has specified alpha argument
+
+   random <- !missing(alpha)
+
+   if (!fixed && !random) {
+
+      ### if both 'fixed' and 'random' are FALSE, obtain CIs for alpha parameters
+
+      cl <- match.call()
+
+      ### total number of non-fixed components
+
+      comps <- sum(!x$alpha.fix)
+
+      if (comps == 0)
+         stop(mstyle$stop("No components for which a CI can be obtained."))
+
+      res.all <- list()
+      j <- 0
+
+      if (any(!x$alpha.fix)) {
+         for (pos in seq_len(x$alphas)[!x$alpha.fix]) {
+            j <- j + 1
+            cl.vc <- cl
+            cl.vc$alpha <- pos
+            cl.vc$time <- FALSE
+            #cl.vc$object <- quote(x)
+            if (verbose)
+               cat(mstyle$verbose(paste("\nObtaining CI for alpha =", pos, "\n")))
+            res.all[[j]] <- eval(cl.vc, envir=parent.frame())
+         }
+      }
+
+      if (.isTRUE(ddd$time)) {
+         time.end <- proc.time()
+         .print.time(unname(time.end - time.start)[3])
+      }
+
+      if (length(res.all) == 1L) {
+         return(res.all[[1]])
+      } else {
+         res.all$digits <- digits
+         class(res.all) <- "list.confint.rma"
+         return(res.all)
+      }
+
+   }
+
+   #########################################################################
+   #########################################################################
+   #########################################################################
+
+   if (random) {
+
+      type <- "PL"
+
+      ######################################################################
+
+      ### check if model actually contains (at least one) such a component and that it was actually estimated
+
+      if (!missing(alpha) && all(x$alpha.fix))
+         stop(mstyle$stop("Model does not contain any estimated 'alpha' components."))
+
+      ### check if user specified more than one alpha component
+
+      if (!missing(alpha) && (length(alpha) > 1L))
+         stop(mstyle$stop("Can only specify one 'alpha' component."))
+
+      ### check if user specified a logical
+
+      if (!missing(alpha) && is.logical(alpha))
+         stop(mstyle$stop("Must specify the number for the 'alpha' component."))
+
+      ### check if user specified a component that does not exist
+
+      if (!missing(alpha) && (alpha > x$alphas || alpha <= 0))
+         stop(mstyle$stop("No such 'alpha' component in the model."))
+
+      ### check if user specified a component that was fixed
+
+      if (!missing(alpha) && x$alpha.fix[alpha])
+         stop(mstyle$stop("Specified 'alpha' component was fixed."))
+
+      ### if everything is good so far, get value of the variance component and set 'comp'
+
+      alpha.pos <- NA
+
+      if (!missing(alpha)) {
+         vc <- x$alpha[alpha]
+         comp <- "alpha"
+         alpha.pos <- alpha
+      }
+
+      #return(list(comp=comp, vc=vc, alpha.pos=alpha.pos))
+
+      ######################################################################
+
+      ### set control parameters for uniroot() and possibly replace with user-defined values
+      ### set vc.min and vc.max and possibly replace with user-defined values
+
+      con <- list(tol=.Machine$double.eps^0.25, maxiter=1000, verbose=FALSE, eptries=10)
+
+      if (comp == "alpha") {
+         if (is.na(x$se.alpha[alpha])) {
+            con$vc.min <- vc/4
+            con$vc.max <- vc*4
+         } else {
+            con$vc.min <- vc - qnorm(.995) * x$se.alpha[alpha]
+            con$vc.max <- vc + qnorm(.995) * x$se.alpha[alpha]
+         }
+      }
+
+      con.pos <- pmatch(names(control), names(con))
+      con[c(na.omit(con.pos))] <- control[!is.na(con.pos)]
+
+      if (verbose)
+         con$verbose <- verbose
+
+      verbose <- con$verbose
+
+      ######################################################################
+
+      vc.lb <- NA
+      vc.ub <- NA
+      ci.null <- FALSE ### logical if CI is a null set
+      lb.conv <- FALSE ### logical if search converged for lower bound (LB)
+      ub.conv <- FALSE ### logical if search converged for upper bound (UB)
+      lb.sign <- ""    ### for sign in case LB must be below vc.min ("<") or above vc.max (">")
+      ub.sign <- ""    ### for sign in case UB must be below vc.min ("<") or above vc.max (">")
+
+      ######################################################################
+      ######################################################################
+      ######################################################################
+
+      ### Profile Likelihood method
+
+      if (type == "PL") {
+
+         if (con$vc.min > vc)
+            stop(mstyle$stop("Lower bound of interval to be searched must be <= estimated value of component."))
+         if (con$vc.max < vc)
+            stop(mstyle$stop("Upper bound of interval to be searched must be >= estimated value of component."))
+
+         objective <- qchisq(1-level, df=1)
+
+         ###################################################################
+
+         ### search for lower bound
+
+         ### get diff value when setting component to vc.min; this value should be positive (i.e., discrepancy must be larger than critical value)
+         ### if it is not, then the lower bound must be below vc.min
+
+         epdiff <- abs(con$vc.min - vc) / con$eptries
+
+         for (i in seq_len(con$eptries)) {
+
+            res <- try(.profile.rma.ls(con$vc.min, obj=x, comp=comp, alpha.pos=alpha.pos, confint=TRUE, objective=objective, verbose=verbose), silent=TRUE)
+
+            if (!inherits(res, "try-error")) {
+
+               if (res < 0) {
+
+                  vc.lb <- con$vc.min
+                  lb.conv <- TRUE
+                  lb.sign <- "<"
+
+               } else {
+
+                  res <- try(uniroot(.profile.rma.ls, interval=c(con$vc.min, vc), tol=con$tol, maxiter=con$maxiter, obj=x, comp=comp, alpha.pos=alpha.pos, confint=TRUE, objective=objective, verbose=verbose, check.conv=TRUE)$root, silent=TRUE)
+
+                  ### check if uniroot method converged
+                  if (!inherits(res, "try-error")) {
+                     vc.lb <- res
+                     lb.conv <- TRUE
+                  }
+
+               }
+
+               break
+
+            }
+
+            con$vc.min <- con$vc.min + epdiff
+
+         }
+
+         if (verbose)
+            cat("\n")
+
+         ###################################################################
+
+         ### search for upper bound
+
+         ### get diff value when setting component to vc.max; this value should be positive (i.e., discrepancy must be larger than critical value)
+         ### if it is not, then the upper bound must be above vc.max
+
+         epdiff <- abs(con$vc.max - vc) / con$eptries
+
+         for (i in seq_len(con$eptries)) {
+
+            res <- try(.profile.rma.ls(con$vc.max, obj=x, comp=comp, alpha.pos=alpha.pos, confint=TRUE, objective=objective, verbose=verbose), silent=TRUE)
+
+            if (!inherits(res, "try-error")) {
+
+               if (res < 0) {
+
+                  vc.ub <- con$vc.max
+                  ub.conv <- TRUE
+                  ub.sign <- ">"
+
+               } else {
+
+                  res <- try(uniroot(.profile.rma.ls, interval=c(vc, con$vc.max), tol=con$tol, maxiter=con$maxiter, obj=x, comp=comp, alpha.pos=alpha.pos, confint=TRUE, objective=objective, verbose=verbose, check.conv=TRUE)$root, silent=TRUE)
+
+                  ### check if uniroot method converged
+                  if (!inherits(res, "try-error")) {
+                     vc.ub <- res
+                     ub.conv <- TRUE
+                  }
+
+               }
+
+               break
+
+            }
+
+            con$vc.max <- con$vc.max - epdiff
+
+         }
+
+         ###################################################################
+
+      }
+
+      ######################################################################
+      ######################################################################
+      ######################################################################
+
+      if (!lb.conv)
+         warning(mstyle$warning("Cannot obtain lower bound of profile likelihood CI due to convergence problems."), call.=FALSE)
+
+      if (!ub.conv)
+         warning(mstyle$warning("Cannot obtain upper bound of profile likelihood CI due to convergence problems."), call.=FALSE)
+
+      ######################################################################
+
+      vc <- c(vc, vc.lb, vc.ub)
+
+      if (comp == "alpha") {
+         res.random <- rbind(vc)
+         if (x$alphas == 1L) {
+            rownames(res.random) <- "alpha"
+         } else {
+            rownames(res.random) <- paste0("alpha.", alpha.pos)
+         }
+      }
+
+      colnames(res.random) <- c("estimate", "ci.lb", "ci.ub")
+
+   }
+
+   #########################################################################
+   #########################################################################
+   #########################################################################
+
+   if (fixed) {
+
+      if (is.element(x$test, c("t"))) {
+         crit <- qt(level/2, df=x$dfs, lower.tail=FALSE)
+      } else {
+         crit <- qnorm(level/2, lower.tail=FALSE)
+      }
+
+      beta  <- c(x$beta)
+      ci.lb <- c(beta - crit * x$se)
+      ci.ub <- c(beta + crit * x$se)
+
+      if (is.function(transf)) {
+         if (is.null(targs)) {
+            beta  <- sapply(beta, transf)
+            ci.lb <- sapply(ci.lb, transf)
+            ci.ub <- sapply(ci.ub, transf)
+         } else {
+            beta  <- sapply(beta, transf, targs)
+            ci.lb <- sapply(ci.lb, transf, targs)
+            ci.ub <- sapply(ci.ub, transf, targs)
+         }
+      }
+
+      ### make sure order of intervals is always increasing
+
+      tmp <- .psort(ci.lb, ci.ub)
+      ci.lb <- tmp[,1]
+      ci.ub <- tmp[,2]
+
+      res.fixed <- cbind(estimate=beta, ci.lb=ci.lb, ci.ub=ci.ub)
+      rownames(res.fixed) <- rownames(x$beta)
+
+   }
+
+   #########################################################################
+   #########################################################################
+   #########################################################################
+
+   res <- list()
+
+   if (fixed)
+      res$fixed <- res.fixed
+
+   if (random)
+      res$random <- res.random
+
+   res$digits <- digits
+
+   if (random) {
+      res$ci.null <- ci.null
+      res$lb.sign <- lb.sign
+      res$ub.sign <- ub.sign
+      #res$vc.min <- con$vc.min
+   }
+
+   if (.isTRUE(ddd$time)) {
+      time.end <- proc.time()
+      .print.time(unname(time.end - time.start)[3])
+   }
+
+   class(res) <- "confint.rma"
+   return(res)
+
+}
--- tests/testthat/test_misc_rma_ls.r
+++ tests/testthat/test_misc_rma_ls.r
@@ -32,21 +32,60 @@ test_that("location-scale model works correctly for two subgroups with different
 
    expect_equivalent(unname(sqrt(diag(solve(res1$hessian[1:2, 1:2])))), res4$se.alpha, tolerance=.tol[["se"]])
 
+   expect_warning(res5 <- rma(yi, vi, data=dat, scale = ~  0 + factor(meta), link="identity"))
+   expect_equivalent(res1$tau2, res5$alpha, tolerance=.tol[["var"]])
+
+   conf1 <- confint(res1)
+   conf5 <- confint(res5, control=list(vc.min=0, vc.max=.5))
+   expect_equivalent(conf1[[1]]$random[1,], conf5[[1]]$random, tolerance=.tol[["var"]])
+   expect_equivalent(conf1[[2]]$random[1,], conf5[[2]]$random, tolerance=.tol[["var"]])
+
 })
 
-test_that("profiling works correctly for location-scale models", {
+test_that("profile() and confint() work correctly for location-scale models", {
 
-   res1 <- rma(yi, vi, data=dat)
-   sav1 <- profile(res1, plot=FALSE, progbar=FALSE, steps=2)
+   opar <- par(no.readonly=TRUE)
 
-   res2 <- rma(yi, vi, data=dat, scale = ~ 1)
-   sav2 <- profile(res2, xlim=log(range(sav1$tau2)), plot=FALSE, progbar=FALSE, steps=2)
+   par(mfrow=c(2,2))
 
-   expect_equivalent(sav1$ll, sav2$ll, tolerance=.tol[["fit"]])
+   res1  <- rma(yi, vi, data=dat)
+   prof1 <- profile(res1, progbar=FALSE, cline=TRUE, xlim=c(.01,.15))
+   conf1 <- confint(res1, type="PL")
+   abline(v=conf1$random[1,2:3], lty="dotted")
+
+   res2  <- rma.mv(yi, vi, random = ~ 1 | id, data=dat)
+   prof2 <- profile(res2, progbar=FALSE, cline=TRUE, xlim=c(.01,.15))
+   conf2 <- confint(res2)
+   abline(v=conf2$random[1,2:3], lty="dotted")
+
+   expect_equivalent(prof1$ll, prof2$ll, tolerance=.tol[["fit"]])
+   expect_equivalent(conf1$random[1,], conf2$random[1,], tolerance=.tol[["var"]])
+
+   res3  <- rma(yi, vi, data=dat, scale = ~ 1)
+   prof3 <- profile(res3, progbar=FALSE, cline=TRUE, xlim=log(c(.01,.15)))
+   conf3 <- confint(res3)
+   abline(v=conf3$random[1,2:3], lty="dotted")
+
+   expect_equivalent(prof1$ll[c(1,20)], prof3$ll[c(1,20)], tolerance=.tol[["fit"]])
+   expect_equivalent(conf1$random[1,], exp(conf3$random), tolerance=.tol[["var"]])
+
+   expect_warning(res4 <- rma(yi, vi, data=dat, scale = ~ 1, link="identity"))
+   prof4 <- profile(res4, progbar=FALSE, cline=TRUE, xlim=c(.01,.15))
+   conf4 <- confint(res4, control=list(vc.max=.2))
+   abline(v=conf4$random[1,2:3], lty="dotted")
+
+   expect_equivalent(prof1$ll, prof4$ll, tolerance=.tol[["fit"]])
+   expect_equivalent(conf1$random[1,], conf4$random, tolerance=.tol[["var"]])
+
+   par(opar)
 
 })
 
 test_that("location-scale model works correctly for a continuous predictor", {
+
+   opar <- par(no.readonly=TRUE)
+
+   par(mfrow=c(3,2))
 
    res1 <- rma(yi, vi, data=dat, scale = ~ grade)
    expect_equivalent(res1$beta, 0.2220791, tolerance=.tol[["coef"]])
@@ -56,11 +95,46 @@ test_that("location-scale model works correctly for a continuous predictor", {
    expect_equivalent(res1$tau2, res2$tau2, tolerance=.tol[["var"]])
 
    res3 <- rma.mv(yi, vi, data=dat, random = ~ sqrt(grade) | id, rho=0, struct="GEN", control=list(hessian=TRUE, vctransf=FALSE))
-
    expect_equivalent(c(res2$alpha), diag(res3$G), tolerance=.tol[["coef"]])
    expect_equivalent(diag(res2$M),  diag(res3$M), tolerance=.tol[["var"]])
-
    expect_equivalent(unname(sqrt(diag(solve(res3$hessian[1:2, 1:2])))), res2$se.alpha, tolerance=.tol[["se"]])
+
+   conf1 <- confint(res1)
+   expect_equivalent(conf1[[1]]$random, c(-3.10513, -5.25032, -1.21713), tolerance=.tol[["var"]])
+   expect_equivalent(conf1[[2]]$random, c( 0.04136, -0.65819,  0.69562), tolerance=.tol[["var"]])
+
+   profile(res1, alpha=1, progbar=FALSE, cline=TRUE)
+   abline(v=conf1[[1]]$random[2:3], lty="dotted")
+   profile(res1, alpha=2, progbar=FALSE, cline=TRUE)
+   abline(v=conf1[[2]]$random[2:3], lty="dotted")
+
+   conf21 <- confint(res2, alpha=1, control=list(vc.min=-0.4, vc.max=0.3))
+   conf22 <- confint(res2, alpha=2, control=list(vc.min=-0.1, vc.max=0.05))
+   conf2  <- list(conf21, conf22)
+   class(conf2) <- "list.confint.rma"
+   expect_equivalent(conf2[[1]]$random, c(0.04293, -0.00137, 0.23145), tolerance=.tol[["var"]])
+   expect_equivalent(conf2[[2]]$random, c(0.00273, -0.04972, 0.04411), tolerance=.tol[["var"]])
+
+   profile(res2, alpha=1, progbar=FALSE, cline=TRUE, xlim=c(0,0.3))
+   abline(v=conf2[[1]]$random[2:3], lty="dotted")
+   profile(res2, alpha=2, progbar=FALSE, cline=TRUE, xlim=c(-0.1,0.05))
+   abline(v=conf2[[2]]$random[2:3], lty="dotted")
+
+   conf3 <- confint(res3)
+   expect_equivalent(conf3[[1]]$random[1,], c(0.04291, 0.00000, 0.11333), tolerance=.tol[["var"]])
+   expect_equivalent(conf3[[2]]$random[1,], c(0.00273, 0.00000, 0.04062), tolerance=.tol[["var"]])
+
+   profile(res3, tau2=1, progbar=FALSE, cline=TRUE, xlim=c(0,.3))
+   abline(v=conf3[[1]]$random[1,2:3], lty="dotted")
+   profile(res3, tau2=2, progbar=FALSE, cline=TRUE, xlim=c(0,.05))
+   abline(v=conf3[[2]]$random[1,2:3], lty="dotted")
+
+   # conf2 and conf3 are not the same because in res3 the two components must
+   # be >= 0 while this restriction does not apply to res2 (and when profiling
+   # or getting the CIs, fixing a particular component can lead to the other
+   # component becoming negative)
+
+   par(opar)
 
 })
 
@@ -73,5 +147,7 @@ test_that("location-scale model works correctly for multiple predictors", {
    expect_warning(res2 <- rma(yi, vi, data=dat, scale = ~ grade + meta + sqrt(ni), control=list(scaleZ=FALSE)))
    expect_equivalent(res2$beta, 0.1110317, tolerance=.tol[["coef"]])
    expect_equivalent(res2$alpha, c(-1.08826210, -0.03429332, 2.09197501, -0.28439156), tolerance=.tol[["coef"]])
+
+   out <- capture.output(print(res1))
 
 })
