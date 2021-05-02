92c494d5fad10987043a57531533b064640929bd
Wolfgang Viechtbauer <wvb@wvbauer.com>
2021-05-02 11:46:18 +0200

    Added 'order' argument to bldiag().

--- R/bldiag.r
+++ R/bldiag.r
@@ -1,4 +1,6 @@
-bldiag <- function(...) {
+bldiag <- function(..., order) {
+
+   mstyle <- .get.mstyle("crayon" %in% .packages())
 
    mlist <- list(...)
 
@@ -9,16 +11,16 @@ bldiag <- function(...) {
    ### make sure each element is a matrix (so that bldiag(matrix(1, nrow=3, ncol=3), 2) also works)
    mlist <- lapply(mlist, function(x) if (inherits(x, "matrix")) x else diag(x, nrow=length(x), ncol=length(x)))
 
-   ### find ?x0 or 0x? matrices
+   ### find any ?x0 or 0x? matrices
    is00 <- sapply(mlist, function(x) any(dim(x) == c(0L,0L)))
 
-   ### filter out those matrices (if there are any)
-   if (any(is00))
-      mlist <- mlist[!is00]
-
-   ### if none are left, return 0x0 matrix
+   ### if all are ?x0 or 0x? matrices, return 0x0 matrix
    if (all(is00))
       return(matrix(nrow=0, ncol=0))
+
+   ### otherwise filter out those matrices (if there are any)
+   if (any(is00))
+      mlist <- mlist[!is00]
 
    csdim <- rbind(c(0,0), apply(sapply(mlist,dim), 1, cumsum)) ### consider using rowCumsums() from matrixStats package
 
@@ -35,6 +37,19 @@ bldiag <- function(...) {
          out[indx[,1],indx[,2]] <- mlist[[i]]
       }
 
+   }
+
+   if (!missing(order)) {
+      if (nrow(out) != ncol(out))
+         stop(mstyle$stop("Can only use 'order' argument for square matrices."))
+      if (length(order) != nrow(out))
+         stop(mstyle$stop(paste0("Length of the 'order' argument (", length(order), ") does not correspond to the dimensions of the matrix (", nrow(out), "x", ncol(out), ").")))
+      if (grepl("^order\\(", deparse(substitute(order)))) {
+         sort.vec <- order
+      } else {
+         sort.vec <- order(order)
+      }
+      out[sort.vec, sort.vec] <- out
    }
 
    return(out)
