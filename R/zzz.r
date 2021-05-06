6bb1793e749fbf1516ea816f9e038cc28e69b2ae
Wolfgang Viechtbauer <wvb@wvbauer.com>
2021-05-06 12:59:42 +0200

    Added dataset dat.dagostino1998.

--- R/zzz.r
+++ R/zzz.r
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkgname) {
 
-   ver <- "2.5-94"
+   ver <- "2.5-95"
 
    loadmsg <- paste0("Loading 'metafor' package (version ", ver, "). For an overview \nand introduction to the package please type: help(metafor)")
 
