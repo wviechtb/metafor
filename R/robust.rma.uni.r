1a36e173da9c4bc710d19e37f50f31d9f34cdc35
Wolfgang Viechtbauer <wvb@wvbauer.com>
2022-03-27 20:44:05 +0200

    Make robust() not available (for now) for 'rma.ls' objects.

--- R/robust.rma.uni.r
+++ R/robust.rma.uni.r
@@ -2,7 +2,7 @@ robust.rma.uni <- function(x, cluster, adjust=TRUE, clubSandwich=FALSE, digits, 
 
    mstyle <- .get.mstyle("crayon" %in% .packages())
 
-   .chkclass(class(x), must="rma.uni", notav=c("rma.uni.selmodel", ifelse(clubSandwich, "rma.ls", NA)))
+   .chkclass(class(x), must="rma.uni", notav=c("rma.uni.selmodel", "rma.ls"))
 
    if (missing(cluster))
       stop(mstyle$stop("Must specify 'cluster' variable."))
