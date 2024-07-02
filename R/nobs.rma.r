8f843a04e7b9dcbbc7dfa6e81f400cd178ff5f09
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-07-02 14:30:39 +0200

    Added 'all' argument to nobs() to obtain more detailed info when set to TRUE.

--- R/nobs.rma.r
+++ R/nobs.rma.r
@@ -1,11 +1,20 @@
-nobs.rma <- function(object, ...) {
+nobs.rma <- function(object, all=FALSE, ...) {
 
    mstyle <- .get.mstyle()
 
    .chkclass(class(object), must="rma")
 
-   #n.obs <- object$k.eff - ifelse(object$method == "REML", 1, 0) * object$p.eff
-   n.obs <- object$k
+   if (all) {
+      n.obs <- c(studies     = object$k,
+                 data        = object$k.all,
+                 subset      = sum(object$subset),
+                 not.na      = sum(object$not.na),
+                 effective   = object$k.eff,
+                 df.residual = object$k.eff - object$p.eff)
+   } else {
+      #n.obs <- object$k.eff - ifelse(object$method == "REML", 1, 0) * object$p.eff
+      n.obs <- object$k
+   }
 
    return(n.obs)
 
